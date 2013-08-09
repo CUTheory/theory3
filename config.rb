05cc76c77371e692ef8e6837113a65b14646231c
Kris Shaffer <kris.shaffer@gmail.com>
2013-08-08 22:46:25 -0500

    added /theory3 to head

--- config.rb
+++ config.rb
@@ -2,10 +2,10 @@
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_path = "/theory3/"
+http_images_path = "/theory3/images"
+http_fonts_path = "/theory3/fonts"
+css_dir = "public/theory3/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
